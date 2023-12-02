9e7729bf2a127dad8f0e6657c6b3d7288fae957c
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-12-02 20:19:41 +0200

    DRY refactor in [task_manager.tests.test_views]

--- task_manager/tests/test_views.py
+++ task_manager/tests/test_views.py
@@ -19,15 +19,29 @@ TEAM_KICK_MEMBER_URL_NAME = "task_manager:team-kick-member"
 PROJECT_CREATE_URL_NAME = "task_manager:project-create"
 
 
-def assert_login_required(test_case_obj: TestCase, url: str) -> None:
-    response = test_case_obj.client.get(url)
-    test_case_obj.assertNotEquals(response.status_code, 200)
+def assert_url_access(
+        self: TestCase,
+        url_name: str,
+        status_code: int = 200,
+        must_equals: bool = True,
+        **kwargs
+):
+    """:returns: response object"""
+    if url_name.startswith("/"):
+        url = url_name
+    else:
+        url = reverse(url_name, kwargs=kwargs)
+    response = self.client.get(url)
+    if must_equals:
+        self.assertEquals(response.status_code, status_code)
+    else:
+        self.assertNotEquals(response.status_code, status_code)
+    return response
 
 
 class PublicHomePageTests(TestCase):
     def test_homepage_using_template(self):
-        response = self.client.get(HOME_PAGE_URL)
-        self.assertEqual(response.status_code, 200)
+        response = assert_url_access(self, HOME_PAGE_URL)
         self.assertTemplateUsed(response, "task_manager/index.html")
 
     def test_homepage_context_data_consists_stats(self):
@@ -46,26 +60,29 @@ class PublicHomePageTests(TestCase):
 
 class PublicUserTests(TestCase):
     def test_user_register_using_template(self):
-        response = self.client.get(USER_REGISTER_URL)
-        self.assertEqual(response.status_code, 200)
+        response = assert_url_access(self, USER_REGISTER_URL)
         self.assertTemplateUsed(response, "registration/register.html")
 
     def assert_user_related_view_login_required(self, url_name: str) -> None:
         user = get_user_model().objects.create(username="test.user")
-        url = reverse(url_name, kwargs={"slug": user.username})
-        assert_login_required(self, url)
+        assert_url_access(
+            self,
+            url_name,
+            must_equals=False,
+            slug=user.username
+        )
 
     def test_user_profile_login_required(self):
         self.assert_user_related_view_login_required(USER_PROFILE_URL_NAME)
 
     def test_user_profile_redirect_login_required(self):
-        assert_login_required(self, USER_PROFILE_REDIRECT_URL)
+        assert_url_access(self, USER_PROFILE_REDIRECT_URL, 200, False)
 
     def test_user_profile_edit_login_required(self):
-        assert_login_required(self, USER_PROFILE_EDIT_URL)
+        assert_url_access(self, USER_PROFILE_EDIT_URL, 200, False)
 
     def test_user_profile_delete_login_required(self):
-        assert_login_required(self, USER_PROFILE_DELETE_URL)
+        assert_url_access(self, USER_PROFILE_DELETE_URL, 200, False)
 
 
 class PrivateUserTests(TestCase):
@@ -77,10 +94,7 @@ class PrivateUserTests(TestCase):
         self.client.force_login(self.user)
 
     def assert_retrieve_user_related_view(self, url_name: str) -> None:
-        response = self.client.get(
-            reverse(url_name, kwargs={"slug": self.user.username})
-        )
-        self.assertEquals(response.status_code, 200)
+        assert_url_access(self, url_name, slug=self.user.username)
 
     def test_retrieve_user_profile_page(self):
         self.assert_retrieve_user_related_view(USER_PROFILE_URL_NAME)
@@ -94,18 +108,16 @@ class PrivateUserTests(TestCase):
         self.assertRedirects(response, expected_url)
 
     def test_retrieve_user_profile_edit_page(self):
-        response = self.client.get(USER_PROFILE_EDIT_URL)
-        self.assertEquals(response.status_code, 200)
+        assert_url_access(self, USER_PROFILE_EDIT_URL)
 
     def test_user_profile_edit_object_is_current_user(self):
-        response = self.client.get(USER_PROFILE_EDIT_URL)
+        response = assert_url_access(self, USER_PROFILE_EDIT_URL)
         context = response.context
         self.assertIn("object", context)
         self.assertEquals(context["object"], self.user)
 
     def test_retrieve_user_profile_delete(self):
-        response = self.client.get(USER_PROFILE_DELETE_URL)
-        self.assertEquals(response.status_code, 200)
+        response = assert_url_access(self, USER_PROFILE_DELETE_URL)
         context_data = response.context
         self.assertIn("object", context_data)
         self.assertEquals(context_data["object"], self.user)
@@ -113,16 +125,15 @@ class PrivateUserTests(TestCase):
 
 class PublicTeamTests(TestCase):
     def test_team_list_login_required(self):
-        assert_login_required(self, TEAM_LIST_URL)
+        assert_url_access(self, TEAM_LIST_URL, 200, False)
 
     def test_team_create_login_required(self):
-        assert_login_required(self, TEAM_CREATE_URL)
+        assert_url_access(self, TEAM_CREATE_URL, 200, False)
 
     def assert_team_related_view_login_required(self, url_name: str) -> None:
         user = get_user_model().objects.create(username="test.user")
         team = Team.objects.create(name="Test team", founder=user)
-        url = reverse(url_name, kwargs={"team_slug": team.slug})
-        assert_login_required(self, url)
+        assert_url_access(self, url_name, 200, False, team_slug=team.slug)
 
     def test_team_detail_login_required(self):
         self.assert_team_related_view_login_required(
@@ -143,14 +154,15 @@ class PublicTeamTests(TestCase):
         user = get_user_model().objects.create(username="test.user")
         team = Team.objects.create(name="Test team", founder=user)
         team.members.add(user)
-        url = reverse(
+        team.save()
+        assert_url_access(
+            self,
             TEAM_KICK_MEMBER_URL_NAME,
-            kwargs={
-                "team_slug": team.slug,
-                "member_username": user.username
-            }
+            200,
+            False,
+            team_slug=team.slug,
+            member_username=user.username
         )
-        assert_login_required(self, url)
 
 
 class PrivateTeamTest(TestCase):
@@ -172,8 +184,7 @@ class PrivateTeamTest(TestCase):
         self.client.force_login(self.user)
 
     def test_retrieve_team_list_page(self):
-        response = self.client.get(TEAM_LIST_URL)
-        self.assertEquals(response.status_code, 200)
+        assert_url_access(self, TEAM_LIST_URL)
 
     def test_team_list_correct_context_data(self):
         response = self.client.get(TEAM_LIST_URL)
@@ -190,13 +201,11 @@ class PrivateTeamTest(TestCase):
         )
 
     def test_team_list_using_template(self):
-        response = self.client.get(TEAM_LIST_URL)
-        self.assertEqual(response.status_code, 200)
+        response = assert_url_access(self, TEAM_LIST_URL)
         self.assertTemplateUsed(response, "task_manager/team_list.html")
 
     def test_retrieve_team_create_page(self):
-        response = self.client.get(TEAM_CREATE_URL)
-        self.assertEquals(response.status_code, 200)
+        assert_url_access(self, TEAM_CREATE_URL)
 
     def assert_retrieve_team_related_view(
             self,
@@ -206,12 +215,7 @@ class PrivateTeamTest(TestCase):
     ) -> None:
         if not slug:
             slug = self.founded_team.slug
-        url = reverse(url_name, kwargs={"team_slug": slug})
-        response = self.client.get(url)
-        if must_pass:
-            self.assertEquals(response.status_code, 200)
-        else:
-            self.assertNotEquals(response.status_code, 200)
+        assert_url_access(self, url_name, 200, must_pass, team_slug=slug)
 
     def test_retrieve_team_detail_page(self):
         self.assert_retrieve_team_related_view(TEAM_DETAIL_URL_NAME)
@@ -247,15 +251,13 @@ class PrivateTeamTest(TestCase):
         )
         team.members.add(several_person)
         team.save()
-        url = reverse(
+        assert_url_access(
+            self,
             TEAM_KICK_MEMBER_URL_NAME,
-            kwargs={
-                "team_slug": team.slug,
-                "member_username": several_person.username
-            }
+            expected_status_code,
+            team_slug=team.slug,
+            member_username=several_person.username
         )
-        response = self.client.get(url)
-        self.assertEquals(response.status_code, expected_status_code)
         if member_must_be_kicked:
             self.assertNotIn(several_person, team.members.all())
 
@@ -281,13 +283,12 @@ class PublicProjectTests(TestCase):
             founder=self.user
         )
 
-    def assert_login_required_with_kwargs(self, url_name, **kwargs):
-        url = reverse(url_name, kwargs=kwargs)
-        assert_login_required(self, url)
-
     def test_project_create_login_required(self):
-        self.assert_login_required_with_kwargs(
+        assert_url_access(
+            self,
             PROJECT_CREATE_URL_NAME,
+            200,
+            False,
             team_slug=self.team.slug
         )
 
@@ -316,29 +317,18 @@ class PrivateProjectTests(TestCase):
         )
         self.client.force_login(self.user)
 
-    def assert_url_access_with_kwargs(
-            self,
-            url_name: str,
-            must_retrieve: bool,
-            **kwargs
-    ):
-        url = reverse(url_name, kwargs=kwargs)
-        response = self.client.get(url)
-        if must_retrieve:
-            self.assertEquals(response.status_code, 200)
-        else:
-            self.assertNotEquals(response.status_code, 200)
-
     def test_retrieve_project_create_page_for_founder(self):
-        self.assert_url_access_with_kwargs(
+        assert_url_access(
+            self,
             PROJECT_CREATE_URL_NAME,
-            True,
             team_slug=self.founded_team.slug
         )
 
     def test_discard_project_create_page_for_not_founder(self):
-        self.assert_url_access_with_kwargs(
+        assert_url_access(
+            self,
             PROJECT_CREATE_URL_NAME,
+            200,
             False,
             team_slug=self.involved_team.slug
         )
